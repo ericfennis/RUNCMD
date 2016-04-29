6a3ed281130749ec313670fe8cfd86abdbf0bf2d
Eric Fennis <eric.fennis@gmail.com>
2016-04-29 11:02:19 +0200

    Adding CMS to repository

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'application' => 
+  array (
+    'debug' => false,
+  ),
+  'database' => 
+  array (
+    'default' => 'sqlite',
+    'connections' => 
+    array (
+      'sqlite' => 
+      array (
+        'prefix' => 'pk_',
+      ),
+    ),
+  ),
+  'system' => 
+  array (
+    'secret' => 'SdDzYcg4v4NrMYH.NKLGxYqlz4AeuQJzvmwHPFje0699WM22957VWec5yV1PbON/',
+  ),
+);
